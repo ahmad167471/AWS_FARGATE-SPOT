952b0a5feecd0ff9ce8833f3815d9c2366858765
Ahmad Amin <ahmad167471.com>
2026-02-24 16:46:55 +0530

    Removed unused vpc_id variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,19 @@
 variable "region" {
-  default = "us-east-1"
+  description = "AWS region"
+  default     = "us-east-1"
 }
 
 variable "image_url" {
-  description = "ECR image URL passed from GitHub Actions"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID for ECS deployment"
+  description = "Full ECR image URL passed from GitHub Actions"
   type        = string
 }
 
 variable "db_username" {
-  default = "strapiuser"
+  description = "Database username"
+  default     = "strapiuser"
 }
 
 variable "db_password" {
-  default = "strapi123!"
+  description = "Database password"
+  default     = "strapi123!"
 }
