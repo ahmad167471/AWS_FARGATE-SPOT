ef67591f6b2ca9289d2db8037aa98a4cf7f568c5
Ahmad Amin <ahmad167471.com>
2026-02-24 14:50:13 +0530

    adding vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,17 @@
+########################################
+# USE DEFAULT VPC
+########################################
+
+data "aws_vpc" "default" {
+  default = true
+}
+########################################
+# GET DEFAULT SUBNETS
+########################################
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
