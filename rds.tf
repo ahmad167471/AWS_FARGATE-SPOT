35b1fe5f806d80a1b3537e3ce6a0d7630aa79ad2
Ahmad Amin <ahmad167471.com>
2026-02-24 12:55:34 +0530

    Initial commit: Strapi + Terraform ECS + RDS stack

---
+++ rds.tf
@@ -0,0 +1,39 @@
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "ahmad-db-subnet-group"
+  subnet_ids = data.aws_subnets.default.ids
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "ahmad-rds-sg"
+  description = "Allow ECS to connect to RDS"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "ahmad_db" {
+  identifier              = "ahmad-db"
+  engine                  = "postgres"
+  instance_class          = "db.t3.micro"
+  allocated_storage       = 20
+  db_name                 = "strapi"
+  username                = var.db_username
+  password                = var.db_password
+  skip_final_snapshot     = true
+  multi_az                = false
+  publicly_accessible     = false
+  db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+}
